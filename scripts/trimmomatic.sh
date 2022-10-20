9684eb194f2dbc9c0739959d31f89c856124a355
Rebecca Clement <33873594+rebeclem@users.noreply.github.com>
2022-10-20 16:42:42 -0400

    added the headcrop to trim 12 bases from beginning of seqs

--- scripts/trimmomatic.sh
+++ scripts/trimmomatic.sh
@@ -17,7 +17,7 @@ echo $name
 
 module load trimmomatic/0.38
 
-java -jar /software/7/apps/trimmomatic/0.38/trimmomatic-0.38.jar PE -threads 10 -phred33 ${name}*_R1_001.fastq.gz ${name}*_R2_001.fastq.gz ${name}_R1_001_trim.fastq.gz ${name}_R1_001_single.fastq.gz ${name}_R2_001_trim.fastq.gz ${name}_R2_001_single.fastq.gz ILLUMINACLIP:/software/7/apps/trimmomatic/0.38/adapters/TruSeq3-PE-2.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+java -jar /software/7/apps/trimmomatic/0.38/trimmomatic-0.38.jar PE -threads 10 -phred33 ${name}*_R1_001.fastq.gz ${name}*_R2_001.fastq.gz ${name}_R1_001_trim.fastq.gz ${name}_R1_001_single.fastq.gz ${name}_R2_001_trim.fastq.gz ${name}_R2_001_single.fastq.gz ILLUMINACLIP:/software/7/apps/trimmomatic/0.38/adapters/TruSeq3-PE-2.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 HEADCROP:12
 
 #---Complete job
 
