dfdb5522a4311f3ff87b84dd78fb83ca3c952756
Rebecca Clement <33873594+rebeclem@users.noreply.github.com>
2022-12-13 23:14:30 -0500

    Create pxcat.sh

---
+++ scripts/pxcat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J pxcat
+#SBATCH -o pxcat_%A_%a.out
+#SBATCH -e pxcat_%A_%a.err
+#SBATCH --nodes=1
+#SBATCH -t 1-00:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=rebeclem@gmail.com
+
+
+module load phyx
+pxcat -s OG*final.fasta -p partitions.txt -o aphis_concat.fasta
