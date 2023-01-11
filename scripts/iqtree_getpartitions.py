aa6523c760ce74f2f379333d62ae2293745b7cc2
Rebecca Clement <33873594+rebeclem@users.noreply.github.com>
2023-01-11 13:55:32 -0500

    Create iqtree_getpartitions.py

---
+++ scripts/iqtree_getpartitions.py
@@ -0,0 +1,15 @@
+from Bio import AlignIO
+import glob
+
+for fasta_file in glob.glob('OG*pal.fasta_mfgb'):
+    alignment = AlignIO.read(open(fasta_file), "fasta")
+    outfile_name = fasta_file.replace('pal.fasta_mfgb', 'parts.txt')
+    outfile = open(outfile_name, 'w')
+    aln_str = str(alignment.get_alignment_length())
+    line1 = 'DNA, part1 = 1-%s\\3\n' % (aln_str)
+    outfile.write(line1)
+    line2 = 'DNA, part2 = 2-%s\\3\n' % (aln_str)
+    outfile.write(line2)
+    line3 = 'DNA, part3 = 3-%s\\3' % (aln_str)
+    outfile.write(line3)
+    outfile.close()
