a0dc162705acaa34532b8b8b1f154540dac10b75
Rebecca Clement <rebeclem@gmail.com>
2023-06-28 09:41:37 -0500

    adding mpileup

---
+++ scripts/mpileup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --job-name=bcf_mpileup
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -t 2-00:00:00
+#SBATCH -p short
+#SBATCH --mail-user=rebeclem@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+hostname
+date
+
+module load bcftools
+
+# Set output name
+outname="aphis"
+
+# Put list of bam files into a file
+ls *sorted.bam >list.bam
+
+# set reference genome location
+GEN=../Aphis_fabae_JIC1_v2.scaffolds.braker.filtered.cds.fa
+
+bcftools mpileup \
+        -f $GEN \
+        -b list.bam \
+        -q 20 \
+        -Q 30 \
+         >${outname}.pileup
+
+bcftools call -m -v -Oz -o ${outname}.vcf.gz ${outname}.pileup
